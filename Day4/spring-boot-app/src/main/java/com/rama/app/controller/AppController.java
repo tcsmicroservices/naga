afdef5fb7615923990aaa168ccdead18ead28122
sfjbs13june <sfjbs13june@gmail.com>
2022-08-25 11:28:12 +0000

    Day4 spring boot docker

--- Day4/spring-boot-app/src/main/java/com/rama/app/controller/AppController.java
+++ Day4/spring-boot-app/src/main/java/com/rama/app/controller/AppController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AppController {	
-	    @RequestMapping("/")
+	    @RequestMapping("/hello")
 	    public String index() {
 	        return "Hello from controller";
 	    }
