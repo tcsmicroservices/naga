58f4746554883c4fb7314aae91da84e04e684214
sfjbs13june <sfjbs13june@gmail.com>
2022-08-29 12:20:11 +0000

    Day6 project

---
+++ Day6/employee-microservice/src/main/java/com/employee/app/Employee.java
@@ -0,0 +1,52 @@
+package com.employee.app;
+
+public class Employee {
+
+    String empId;
+    String empName;
+    double salary;
+    String department;
+
+    public Employee(String empId, String empName, double salary, String department) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.department = department;
+    }
+    public Employee(){
+
+    }
+
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpid(String empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
